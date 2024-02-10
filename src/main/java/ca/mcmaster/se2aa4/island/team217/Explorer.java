6a511448a489cabf2484e91bdcad7c6adedb3097
Ayush Patel <ayushpatel.2026@gmail.com>
2024-02-09 20:12:59 -0500

    hashmap to store responses (done as group)

--- src/main/java/ca/mcmaster/se2aa4/island/team217/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team217/Explorer.java
@@ -9,24 +9,33 @@ import org.apache.logging.log4j.Logger;
 import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.json.JSONArray;
 
 public class Explorer implements IExplorerRaid {
 
     enum Heading {
-        NORTH, EAST, SOUTH, WEST
+        N, E, S, W
     }
 
-    Heading currentHeading = Heading.EAST;
+    Heading currentHeading = Heading.E;
+
+    private String action; // the action to be taken
 
     private final Logger logger = LogManager.getLogger();
 
     private JSONObject checker = new JSONObject(); // to store values to check for iteration
+    private JSONObject newResponse = new JSONObject(); // to store values to check for iteration
 
     private Integer distToGround = 0;
     private Boolean foundLand = false;
     private Boolean boilerPlate = true;
     private Boolean flyCheck = false;
     private Boolean end = false;
+    private Boolean boilerPlateMission = true;
+
+    Drone drone;
+    MapRepresenter map;
+    MissionControl missionControl = new MissionControl(drone, map);
 
     @Override
     public void initialize(String s) {
@@ -35,9 +44,13 @@ public class Explorer implements IExplorerRaid {
         logger.info("** Initialization info:\n {}", info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
+        drone = new Drone(batteryLevel, direction);
+        map = new MapRepresenter();
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
     }
+
+    
 
     @Override
     public String takeDecision() {
@@ -47,6 +60,7 @@ public class Explorer implements IExplorerRaid {
         if (boilerPlate) {
             parameters.put("direction", "S");
             decision.put("action", "echo");
+            action = "echo";
             decision.put("parameters", parameters);
             boilerPlate = false;
             logger.info("** Decision: {}", decision.toString());
@@ -56,36 +70,45 @@ public class Explorer implements IExplorerRaid {
         if (!flyCheck && !foundLand) {
             switch (checker.getJSONObject("check").getString("found")) {
                 case "OUT_OF_RANGE":
-
                     decision.put("action", "fly");
+                    action = "fly";
                     flyCheck = true;
                     logger.info("** Decision: {}", decision.toString());
                     return decision.toString();
                 case "GROUND":
                     distToGround = checker.getJSONObject("check").getInt("range");
                     foundLand = true;
+
+                    // change heading to south here so we can fly towards ground
                     parameters.put("direction", "S");
                     decision.put("action", "heading");
                     decision.put("parameters", parameters);
+                    action = "heading";
                     logger.info("** Decision: {}", decision.toString());
                     return decision.toString();
                 default:
                     break;
             }
-        } else {
+        } else if (flyCheck) {
+            //echo south again so we can see if there is ground in that direction, only do this if the previous echo was out of range (flyCheck = true)
             parameters.put("direction", "S");
             decision.put("action", "echo");
             decision.put("parameters", parameters);
+            action = "echo";
             flyCheck = false;// cechl
         }
 
         if (foundLand && distToGround != 0) {
+            // fly south until we reach the ground
             decision.put("action", "fly");
+            action = "fly";
             distToGround--;
         }
         if (distToGround == 0 && foundLand && !end) {
+            // once we found the ground, we need to scan it so that is shows up on the svg map
             // parameters.put("direction", "S");
             decision.put("action", "scan");
+            action = "scan";
             // decision.put("parameters", parameters);
             end = true;
             logger.info("** Decision: {}", decision.toString());
@@ -93,7 +116,25 @@ public class Explorer implements IExplorerRaid {
         }
 
         if (end) {
+            // Get the JSONArray from the JSONObject
+            JSONArray biomesArray = checker.getJSONObject("check").getJSONArray("biomes");
+
+            // Initialize a String array with the same length as the JSONArray
+            String[] biomes = new String[biomesArray.length()];
+
+            // Iterate through the JSONArray and extract each element as a String
+            for (int i = 0; i < biomesArray.length(); i++) {
+                biomes[i] = biomesArray.getString(i);
+            }
+
+            return decision.toString(); 
+
+        }
+
+        if (end) {
+            // ground has been found so return to base
             decision.put("action", "stop");
+            action = "stop";
 
         }
 
@@ -104,6 +145,13 @@ public class Explorer implements IExplorerRaid {
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        if (boilerPlateMission) {
+            logger.info("Mission has started");
+            logger.info("Mission has started");
+            boilerPlateMission = false;
+        }
+        missionControl.startMission(action, response);
+
         logger.info("** Response received:\n" + response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
