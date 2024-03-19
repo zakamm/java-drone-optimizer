ffa881b7e293030ca39ab20b2ea856f75d755f87
Ayush Patel <ayushpatel.2026@gmail.com>
2024-03-19 10:03:48 -0400

    moved store response method to ResponseStorage (close #34)

--- src/main/java/ca/mcmaster/se2aa4/island/team217/ResponseStorage.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team217/ResponseStorage.java
@@ -1,6 +1,10 @@
 package ca.mcmaster.se2aa4.island.team217;
 
 import java.util.List;
+import java.util.ArrayList;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.JSONArray;
 
 public class ResponseStorage {
    
@@ -70,6 +74,55 @@ public class ResponseStorage {
         this.biomes = null;
         this.creeks = null;
         this.site = null;
+    }
+
+    public void storeResponse(String action, JSONObject previousResponse) {
+        // want to clear at the start of each iteration, sets all values to null
+        clear();
+
+        // all actions will have cost
+        List<String> temp = new ArrayList<String>();
+        setCost(previousResponse.getInt("cost"));
+
+        if (action.equals("echo")) {
+            setRange(previousResponse.getJSONObject("extras").getInt("range"));
+            setFound(previousResponse.getJSONObject("extras").getString("found"));
+        }
+
+        // store as lists with first item being null if empty
+        else if (action.equals("scan")) {
+            temp = new ArrayList<String>();
+            JSONArray creeksArray = previousResponse.getJSONObject("extras").getJSONArray("creeks");
+            if (creeksArray.length() == 0) {
+                temp.add("null");
+            } else {
+                for (int i = 0; i < creeksArray.length(); i++) {
+                    temp.add(creeksArray.getString(i));
+                }
+            }
+            setCreeks(temp);
+
+            temp = new ArrayList<String>();
+            JSONArray biomesArray = previousResponse.getJSONObject("extras").getJSONArray("biomes");
+            if (biomesArray.length() == 0) {
+                temp.add("null");
+            } else {
+                for (int i = 0; i < biomesArray.length(); i++) {
+                    temp.add(biomesArray.getString(i));
+                }
+            }
+            setBiomes(temp);
+
+            // assuming there is only one site, we only store the first value
+            temp = new ArrayList<String>();
+            JSONArray sitesArray = previousResponse.getJSONObject("extras").getJSONArray("sites");
+            if (sitesArray.length() == 0) {
+                temp.add("null");
+            } else {
+                temp.add(sitesArray.getString(0));
+            }
+            setSite(temp.get(0));
+        }
     }
 
 
