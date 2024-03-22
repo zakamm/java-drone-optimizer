7b231615815ed81203e08e7fa6be757524eaed53
Mohid Zaka <mohidzaka3@gmail.com>
2024-03-22 14:50:17 -0400

    create classes for some tests

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team217/FindingGroundStages/FlyToGroundTest.java
@@ -0,0 +1,4 @@
+package ca.mcmaster.se2aa4.island.team217.FindingGroundStages;
+
+public class FlyToGroundTest {
+}
---
+++ src/test/java/ca/mcmaster/se2aa4/island/team217/MapRepresentation/MissionControlTest.java
@@ -0,0 +1,4 @@
+package ca.mcmaster.se2aa4.island.team217.MapRepresentation;
+
+public class MissionControlTest {
+}
