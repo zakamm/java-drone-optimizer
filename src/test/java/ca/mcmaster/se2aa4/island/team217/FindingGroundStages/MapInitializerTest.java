3ed8e1982715e2f35bcacaf10ca2e12c3452dd32
Waqar Ul-Hassan <waqarulhassan7@gmail.com>
2024-03-22 23:14:52 -0400

    MapInitializer Test cases complete

--- src/test/java/ca/mcmaster/se2aa4/island/team217/FindingGroundStages/MapInitializerTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team217/FindingGroundStages/MapInitializerTest.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.se2aa4.island.team217.FindingGroundStages;
 
 import ca.mcmaster.se2aa4.island.team217.MapRepresentation.MapRepresenter;
+import ca.mcmaster.se2aa4.island.team217.FindingGroundStages.MapInitializer;
 import ca.mcmaster.se2aa4.island.team217.Drone;
 import ca.mcmaster.se2aa4.island.team217.Heading;
 
@@ -11,54 +12,54 @@ import java.util.*;
 
 public class MapInitializerTest {
 
-    // @Test
-    // void testInitializeMapDimensions() {
-    // MapInitializer map = new MapInitializer(Drone.getInstance(1000, "N",
-    // MapRepresenter.getInstance()),
-    // MapRepresenter.getInstance());
-    // Heading h = Heading.E;
-    // Integer range = 52;
+    @Test
+    void testInitializeMapDimensions() {
+        MapInitializer map = new MapInitializer(Drone.getInstance(1000, "N",
+                MapRepresenter.getInstance()),
+                MapRepresenter.getInstance());
+        Heading h = Heading.E;
+        Integer range = 52;
 
-    // map.initializeMapDimensions(h, range);
+        map.initializeMapDimensions(h, range);
 
-    // assertEquals(Heading.E, h);
-    // assertEquals(52, range);
-    // }
+        assertEquals(Heading.E, h);
+        assertEquals(52, range);
+    }
 
-    // @Test
-    // void testInitializeRowsAndColumns() {
-    // MapInitializer map = new MapInitializer(Drone.getInstance(1000, "N",
-    // MapRepresenter.getInstance()),
-    // MapRepresenter.getInstance());
+    @Test
+    void testInitializeRowsAndColumns() {
+        MapInitializer map = new MapInitializer(Drone.getInstance(1000, "N",
+                MapRepresenter.getInstance()),
+                MapRepresenter.getInstance());
 
-    // map.setTopRows(0);
-    // map.setBottomRows(52);
-    // map.setLeftColumns(0);
-    // map.setRightColumns(52);
+        map.topRows = 0;
+        map.bottomRows = 52;
+        map.leftColumns = 0;
+        map.rightColumns = 52;
 
-    // map.initializeRowsAndColumns();
+        map.initializeRowsAndColumns();
 
-    // assertEquals(map.getBottomRows() + map.getTopRows() + 1,
-    // MapRepresenter.getInstance().rows);
-    // assertEquals(map.getRightColumns() + map.getLeftColumns() + 1,
-    // MapRepresenter.getInstance().columns);
+        assertEquals(map.bottomRows + map.topRows + 1,
+                MapRepresenter.getInstance().rows);
+        assertEquals(map.rightColumns + map.leftColumns + 1,
+                MapRepresenter.getInstance().columns);
+    }
 
-    // }
+    @Test
+    void testDirectionToEcho() {
+        MapInitializer map = new MapInitializer(Drone.getInstance(1000, "N",
+                MapRepresenter.getInstance()),
+                MapRepresenter.getInstance());
 
-    // @Test
-    // void testDirectionToEcho() {
-    // MapInitializer map = new MapInitializer(Drone.getInstance(1000, "N",
-    // MapRepresenter.getInstance()),
-    // MapRepresenter.getInstance());
+        Heading h = Heading.E;
+        map.topRows = 0;
+        map.bottomRows = 52;
+        map.leftColumns = 0;
+        map.rightColumns = 52;
 
-    // Heading h = Heading.E;
-    // map.setTopRows(0);
-    // map.setBottomRows(52);
-    // map.setLeftColumns(0);
-    // map.setRightColumns(52);
+        map.directionToEcho(h);
 
-    // map.directionToEcho(h);
+        assertEquals(Heading.E, h);
+    }
 
-    // assertEquals(Heading.E, h);
-    // }
 }
