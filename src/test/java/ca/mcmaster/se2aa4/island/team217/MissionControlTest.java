3b69d655bb107bff4b036423924c9d96c611e62b
Mohid Zaka <mohidzaka3@gmail.com>
2024-03-24 05:12:25 -0400

    added more test cases for missioncontrol

--- src/test/java/ca/mcmaster/se2aa4/island/team217/MissionControlTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team217/MissionControlTest.java
@@ -1,5 +1,7 @@
 package ca.mcmaster.se2aa4.island.team217;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import ca.mcmaster.se2aa4.island.team217.MapRepresentation.MapRepresenter;
@@ -16,5 +18,29 @@ public class MissionControlTest {
         String decision = instance.nextDecision();
         assertNotNull(decision);
     }
+    @Test
+    public void testNextDecisionWithLowBattery() {
+        Drone drone = new Drone(49, "E", new MapRepresenter());
+        MapRepresenter map = new MapRepresenter();
+        MissionControl instance = new MissionControl(drone, map);
+        String decision = instance.nextDecision();
+        assertEquals("{\"action\": \"stop\"}", decision);
+    }
+    @Test
+    public void testNextDecisionWithNoBattery() {
+        Drone drone = new Drone(0, "E", new MapRepresenter());
+        MapRepresenter map = new MapRepresenter();
+        MissionControl instance = new MissionControl(drone, map);
+        String decision = instance.nextDecision();
+        assertEquals("{\"action\": \"stop\"}", decision);
+    }
 
+    @Test
+    public void testNextDecisionWithHighBattery() {
+        Drone drone = new Drone(100, "E", new MapRepresenter());
+        MapRepresenter map = new MapRepresenter();
+        MissionControl instance = new MissionControl(drone, map);
+        String decision = instance.nextDecision();
+        assertNotNull(decision);
+    }
 }
